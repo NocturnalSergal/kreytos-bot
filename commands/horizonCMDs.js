159f1ee18dfa18933def4abf29fba44e03efdb53
Thomas Waller <31709622+Thomas-Waller@users.noreply.github.com>
2017-11-12 21:02:43 -0500

    Add files via upload

---
+++ commands/horizonCMDs.js
@@ -0,0 +1,75 @@
+const https = require("https");
+
+let msIP = "192.99.160.99:25560";
+
+function list(msg) {
+  https.get(url, function(res) {
+    let body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      let result = JSON.parse(body)
+      //msg.channel.send("There are " + result.players.online + " players online");
+      let playersTOT = "";
+      for (let i = 0; i < result.players.sample.length; i++) {
+        playersTOT += result.players.sample[i].name + ', ';
+      }
+      //if (playersTOT < 1) return;
+
+      if (playersTOT < 1) {
+        msg.channel.send("There are no players online");
+      } else {
+        msg.channel.send("There are " + result.players.online + " players online. " + playersTOT);
+      }
+
+    });
+  }).on('error', function(e) {
+    console.log("Error: " + e.message);
+  });
+}
+
+function stats(msg) {
+  https.get(url, function(res) {
+    msg.channel.send("Horizon's III server is up and running!")
+  }).on('error', function(e) {
+    msg.channel.send("Horizon's III server is offline");
+  });
+}
+
+function motd(msg) {
+  https.get(url, function(res) {
+    let body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      let result = JSON.parse(body)
+      msg.channel.send("Message of the Day : " + result.description);
+    });
+  }).on('error', function(e) {
+    console.log("Error: " + e.message);
+  });
+}
+
+function maxplayers(msg) {
+  https.get(url, function(res) {
+    let body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      let result = JSON.parse(body)
+      msg.channel.send("Max players : " + result.players.max);
+    });
+  }).on('error', function(e) {
+    console.log("Error: " + e.message);
+  });
+}
+
+exports.list = list;
+exports.stats = stats;
+exports.motd = motd;
+exports.maxplayers = maxplayers;
+
+var url = "https://api.minetools.eu/ping/192.99.160.99/25560";
